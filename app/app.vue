b92f6bd80116df29045cc601f2b5596c8dbd05cd
trisnawyd <trisnaw@travlr.com>
2025-10-27 14:58:21 +0800

    feat: integrate Vercel Analytics for improved tracking

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,10 @@
 <template>
   <NuxtLayout>
+    <Analytics />
     <NuxtPage :key="$route.fullPath" />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+import { Analytics } from "@vercel/analytics/nuxt";
+</script>
